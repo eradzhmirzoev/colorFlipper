433617e942bda4e24655c75be086806a5b6ccb0c
Eradzh Mirzoiev <eradzh.m@gmail.com>
2022-11-09 18:26:00 +0100

    created for myself

---
+++ js/limited.js
@@ -0,0 +1,17 @@
+const arrOfColors = ['red', 'blue', 'green', 'yellow'];
+
+const btn = document.querySelector('.btn');
+const valueColor = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+    let index = getRandomNumber();
+    
+    let color = arrOfColors[index];
+
+    document.querySelector('body').style.backgroundColor = color;
+    document.querySelector('.color').textContent = color;
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * arrOfColors.length);
+}
